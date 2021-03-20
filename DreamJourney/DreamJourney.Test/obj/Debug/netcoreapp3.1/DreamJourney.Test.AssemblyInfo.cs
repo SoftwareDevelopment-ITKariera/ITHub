bf865ccf2f33ad7d1135d6bbaa3b4eb4dc3cba4d
Sheyda03 <sheyda.nebi@abv.bg>
2021-03-20 19:01:48 +0200

    Test

--- DreamJourney/DreamJourney.Test/obj/Debug/netcoreapp3.1/DreamJourney.Test.AssemblyInfo.cs
+++ DreamJourney/DreamJourney.Test/obj/Debug/netcoreapp3.1/DreamJourney.Test.AssemblyInfo.cs
@@ -11,16 +11,6 @@
 using System;
 using System.Reflection;
 
-[assembly: Microsoft.AspNetCore.Mvc.Testing.WebApplicationFactoryContentRootAttribute("DreamJourney.Data, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", "C:\\Users\\sheydi\\Desktop\\IT_Kariera\\DreamJourney_Repository\\ITHub\\DreamJourney\\Dre" +
-    "amJourney.Data", "DreamJourney.Data.csproj", "0")]
-[assembly: Microsoft.AspNetCore.Mvc.Testing.WebApplicationFactoryContentRootAttribute("DreamJourney.Data.Models, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", "C:\\Users\\sheydi\\Desktop\\IT_Kariera\\DreamJourney_Repository\\ITHub\\DreamJourney\\Dre" +
-    "amJourney.Data.Models", "DreamJourney.Data.Models.csproj", "0")]
-[assembly: Microsoft.AspNetCore.Mvc.Testing.WebApplicationFactoryContentRootAttribute("DreamJourney, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", "C:\\Users\\sheydi\\Desktop\\IT_Kariera\\DreamJourney_Repository\\ITHub\\DreamJourney\\Dre" +
-    "amJourney", "DreamJourney.csproj", "0")]
-[assembly: Microsoft.AspNetCore.Mvc.Testing.WebApplicationFactoryContentRootAttribute("DreamJourney.Services, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", "C:\\Users\\sheydi\\Desktop\\IT_Kariera\\DreamJourney_Repository\\ITHub\\DreamJourney\\Dre" +
-    "amJourney.Services", "DreamJourney.Services.csproj", "0")]
-[assembly: Microsoft.AspNetCore.Mvc.Testing.WebApplicationFactoryContentRootAttribute("DreamJourney.ViewModels, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", "C:\\Users\\sheydi\\Desktop\\IT_Kariera\\DreamJourney_Repository\\ITHub\\DreamJourney\\Dre" +
-    "amJourney.ViewModels", "DreamJourney.ViewModels.csproj", "0")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("DreamJourney.Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
